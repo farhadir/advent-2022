cd891f755435cde2a594075d617517bddf9a191f
Farhad Irannejad <farhad.irannejad@cern.ch>
2022-12-01 15:03:15 +0100

    Add top 3 for day1

--- src/day1.rs
+++ src/day1.rs
@@ -11,7 +11,7 @@ fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 
 pub fn day1() {
     let mut s = BTreeSet::new();
-    if let Ok(lines) = read_lines("../day1_input.txt") {
+    if let Ok(lines) = read_lines("day1_input.txt") {
         let mut energy: u64 = 0;
         for line in lines {
             if let Ok(ip) = line {
@@ -25,4 +25,7 @@ pub fn day1() {
         }
     }
     println!("Last element is {}", s.iter().next_back().unwrap());
+
+    let mut iter = s.iter();
+    println!("Top 3 {}", iter.next_back().unwrap() + iter.next_back().unwrap() + iter.next_back().unwrap());
 }
