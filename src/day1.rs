b03d1fc75ac741e7c4d8c96fb09a5b7fbd767d48
Farhad Irannejad <farhad.irannejad@cern.ch>
2022-12-02 14:22:36 +0100

    structure

--- src/day1.rs
+++ src/day1.rs
@@ -1,31 +1,25 @@
-use std::collections::BTreeSet;
+use std::collections::{BinaryHeap};
 use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
+use std::io::{BufReader, Lines};
 
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-    where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
-}
 
-pub fn day1() {
-    let mut s = BTreeSet::new();
-    if let Ok(lines) = read_lines("day1_input.txt") {
-        let mut energy: u64 = 0;
-        for line in lines {
-            if let Ok(ip) = line {
-                if ip.is_empty() {
-                    s.insert(energy);
-                    energy = 0;
-                } else {
-                    energy += ip.parse::<u64>().unwrap();
-                }
+pub fn day1(lines: Lines<BufReader<File>>) {
+    let mut s = BinaryHeap::new();
+    let mut energy: u64 = 0;
+    for line in lines {
+        if let Ok(ip) = line {
+            if ip.is_empty() {
+                s.push(energy);
+                energy = 0;
+            } else {
+                energy += ip.parse::<u64>().unwrap();
             }
         }
     }
-    println!("Last element is {}", s.iter().next_back().unwrap());
 
-    let mut iter = s.iter();
-    println!("Top 3 {}", iter.next_back().unwrap() + iter.next_back().unwrap() + iter.next_back().unwrap());
+    println!("Last element is {}", s.peek().unwrap());
+
+    // let test : u64 = s.into_iter().take(3).sum();
+    let top3: u64 = s.pop().unwrap() + s.pop().unwrap() + s.pop().unwrap();
+    println!("Top 3 {}", top3);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
 mod day1;
+mod day2;
+
+use std::path::Path;
+use std::fs::File;
+use std::io::{self, BufRead};
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+    where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
 
 fn main() {
-    day1::day1()
+    // day1::day1(read_lines("day1_input.txt").unwrap());
+    day2::day2(read_lines("day2_input.txt").unwrap());
 }
